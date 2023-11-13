b068390eedc9d1228c300155382bef49e3d36825
Rafael Patel <rafiepatel@gmail.com>
2023-11-12 16:19:01 -0800

    Add Auth0 authentication.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes } from "@generouted/react-router";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import "normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Routes />
+        <Auth0Provider
+            domain="codify.auth0.com"
+            clientId="hAvOsj1ARqX9uXvhmd55bJt007ipcFEC"
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+                audience: "fitness-tracker-dev",
+            }}
+            cacheLocation="memory"
+        >
+            <Routes />
+        </Auth0Provider>
     </React.StrictMode>,
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,53 @@
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import PageWrapper from "../components/PageWrapper";
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    return (
+        <button
+            onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+        >
+            Log Out
+        </button>
+    );
+};
+
+const Profile = () => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
+    return (
+        isAuthenticated &&
+        user && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+        )
+    );
+};
 
 function App() {
     return (
         <PageWrapper>
+            <LoginButton />
+            <Profile />
             <Link to="/workout">Start Workout</Link>
+            <LogoutButton />
         </PageWrapper>
     );
 }
